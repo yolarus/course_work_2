de69b73786e65a8c2d41ecb1ae680aff1ec8887a
Аляутдинов Руслан <rusalyautdinov@yandex.ru>
2024-08-29 19:37:32 +0300

    add vacancy.py

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 from src.head_hunter_api import HeadHunterAPI
-
+from src.vacancy import Vacancy
 
 if __name__ == "__main__":
 
     head_hunter_api = HeadHunterAPI()
 
-    vacancies = head_hunter_api.get_vacancies("python", 25)
+    full_vacancies = head_hunter_api.get_vacancies("python", 5)
 
-    print(vacancies)
-    print(len(vacancies))
+    print(full_vacancies)
+    print(len(full_vacancies))
+    print()
+
+    vacancies = []
+
+    for item in full_vacancies:
+        vacancies.append(Vacancy(item["name"],
+                                 item["url"],
+                                 f'{item["salary"]["from"] if item["salary"]["from"] else item["salary"]["to"]}'
+                                 f' - {item["salary"]["to"] if item["salary"]["to"] else item["salary"]["from"]}'
+                                 f' - {item["salary"]["currency"]}',
+                                 item["snippet"]["responsibility"],
+                                 item["snippet"]["requirement"],
+                                 item["area"]["name"]))
+
+    for item in vacancies:
+        print(item.name)
+        print(item.url)
+        print(item.salary)
+        print(item.short_description)
+        print(item.requirements)
+        print(item.area)
+        print()
+
+    print(vacancies[0] == vacancies[1])
+    print(vacancies[0] == vacancies[0])
+    print(vacancies[2] != vacancies[3])
+    print(vacancies[2] > vacancies[3])
+    print(vacancies[2] >= vacancies[3])
+    print(vacancies[2] < vacancies[3])
+    print(vacancies[2] <= vacancies[3])
---
+++ src/vacancy.py
@@ -0,0 +1,72 @@
+"""
+Создать класс для работы с вакансиями. В этом классе самостоятельно определить атрибуты,
+такие как название вакансии, ссылка на вакансию, зарплата, краткое описание или требования и т. п.
+(всего не менее четырех атрибутов).
+Класс должен поддерживать методы сравнения вакансий между собой по зарплате и валидировать данные,
+которыми инициализируются его атрибуты.
+Способами валидации данных может быть проверка, указана или нет зарплата.
+В этом случае выставлять значение зарплаты 0 или «Зарплата не указана» в зависимости от структуры класса.
+"""
+
+
+class Vacancy:
+    """
+    Класс для работы с вакансиями
+    """
+    __slots__ = ("name", "url", "salary", "short_description", "requirements", "area")
+    name: str
+    url: str
+    salary: str
+    short_description: str
+    requirements: str
+    area: str
+
+    def __init__(self, name: str, url: str, salary: str, short_description: str, requirements: str, area: str) -> None:
+        """
+        Конструктор объектов
+        """
+        self.name = name
+        self.url = url
+        self.salary = salary
+        self.short_description = short_description
+        self.requirements = requirements
+        self.__check_city(area)
+
+    def __check_city(self, area: str) -> None:
+        """
+        Валидация вакансии по месту работы
+        """
+        if area == "Москва":
+            self.area = area
+        else:
+            self.area = "Можно не рассматривать данную вакансию"
+
+    def __eq__(self, other: "Vacancy") -> bool:   # type: ignore[override]
+        """
+        Метод для проверки зарплат вакансий - равно
+        """
+        if (self.salary.split(" - ")[0] == other.salary.split(" - ")[0]
+                and self.salary.split(" - ")[2] == other.salary.split(" - ")[2]):
+            return True
+        else:
+            return False
+
+    def __lt__(self, other: "Vacancy") -> bool:
+        """
+        Метод для проверки зарплат вакансий - меньше
+        """
+        if (self.salary.split(" - ")[0] < other.salary.split(" - ")[0]
+                and self.salary.split(" - ")[2] == other.salary.split(" - ")[2]):
+            return True
+        else:
+            return False
+
+    def __le__(self, other: "Vacancy") -> bool:
+        """
+        Метод для проверки зарплат вакансий - меньше или равно
+        """
+        if (self.salary.split(" - ")[0] <= other.salary.split(" - ")[0]
+                and self.salary.split(" - ")[2] == other.salary.split(" - ")[2]):
+            return True
+        else:
+            return False
